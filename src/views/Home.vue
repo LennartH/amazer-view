2bf3e60f15c10ae75c1e73bfb12f384be2f0d6d8
Lennart Hensler <lennart.hensler@dataqube.de>
2019-11-29 11:25:17 +0100

    Rename HelloWorld to Generator

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Generator msg="Amazer"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Generator from '@/components/Generator.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Generator
   }
 }
 </script>
