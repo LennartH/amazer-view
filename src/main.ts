f53a621b58ab9eb29dee83d218d4ff00ace86d25
Lennart Hensler <lennart.hensler@dataqube.de>
2019-12-06 09:11:17 +0100

    First steps with bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import BoostrapVue from "bootstrap-vue";
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(BoostrapVue);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
