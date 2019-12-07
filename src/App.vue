31d8ec897639c69d8a0dbe8cdf899c5a03c9d050
Lennart Hensler <lennarthensler@gmail.de>
2019-12-07 20:27:31 +0100

    Fix navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="success">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-container>
         <b-navbar-brand href="#">Amazer</b-navbar-brand>
-        <b-link to="/">Home</b-link> |
-        <b-link to="/about">About</b-link>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
       </b-container>
     </b-navbar>
-    <router-view/>
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
