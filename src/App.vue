c4ec2789e4b965864ce83781dee881a355f51a54
Lennart Hensler <lennart.hensler@dataqube.de>
2019-12-13 16:15:55 +0100

    Add config and link to docs

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item to="/">Home</b-nav-item>
-            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item :href="config.docsUrl" target="_blank">Documentation</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
@@ -16,6 +16,18 @@
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import config from "@/config";
+
+export default {
+  data() {
+    return {
+      config: config
+    }
+  }
+}
+</script>
 
 <style scoped>
 .amazer-root {
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import { Dict } from "amazer";
+
+const config: Dict<any> = {
+    docsUrl: "https://docs.a-mazer.de",
+    // docsUrl: "file:///home/lennart/git/projects/amazer-ts/docs/index.html",
+};
+
+export default config;
